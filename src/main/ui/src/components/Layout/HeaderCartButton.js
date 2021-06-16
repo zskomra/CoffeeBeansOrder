89a3d89909d3d9f393ceda3ea9071350413c2aea
z.skomra <z.skomraa@gmail.com>
2021-06-17 01:49:17 +0200

    feat :: add bump when add products

--- src/main/ui/src/components/Layout/HeaderCartButton.js
+++ src/main/ui/src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHigligted, setBtnIsHigligted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const{items}  = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnIsHigligted && classes.bump}`;
+
+  useEffect(() => {
+    if(items.length === 0) {
+      return;
+    };
+    setBtnIsHigligted(true);
+    const timer = setTimeout(()=> {
+      setBtnIsHigligted(false);
+    },300);
+    return () => {
+      clearTimeout(timer);
+    };
+  },[items]);
 
   const numOfCartItems = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount;
   }, 0);
 
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
